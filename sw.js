2e56aab6a3881b5f31207156aa518716b0bc858a
yesanovice <arkokazi08@gmail.com>
2025-05-16 23:57:42 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,21 @@
-const CACHE_NAME = 'daily-nest-cache-v2'; // Bump this version when deploying updates
+const CACHE_NAME = 'daily-nest-cache-v3';
 const CACHE_FILES = [
-  './',
-  './index.html',
-  './icon-192x192.png',
-  './icon-512x512.png'
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
+  '/manifest.json',
+  '/logo-192.png',
+  '/logo-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(CACHE_FILES))
-      .then(() => self.skipWaiting()) // Activate immediately
+      .then(() => self.skipWaiting())
   );
 });
 
@@ -20,11 +25,11 @@ self.addEventListener('activate', (event) => {
       return Promise.all(
         keys.map(key => {
           if (key !== CACHE_NAME) {
-            return caches.delete(key); // Delete old cache
+            return caches.delete(key);
           }
         })
       );
-    }).then(() => self.clients.claim()) // Control all tabs immediately
+    }).then(() => self.clients.claim())
   );
 });
 
